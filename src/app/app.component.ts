81dc442f506d5b1273bb8eaaaf29b4158ea8ca25
José Aguilera <neojal@hotmail.com>
2017-09-12 15:44:36 -0500

    Parte 2 - List of selectables, ngFor, ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+const HEROES: Hero[] = [
+  {id: 1, name: 'The Coon'},
+  {id: 2, name: 'Mysterion'},
+  {id: 3, name: 'Human Kite'},
+  {id: 4, name: 'Toolshed'},
+  {id: 5, name: 'Mosquito'},
+  {id: 6, name: 'Tupperware'},
+  {id: 7, name: 'Iron Maiden'},
+  {id: 8, name: 'Mint-Berry Crunch'}
+];
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,14 +24,10 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Tour of Heroes - The SouthPark Series';
+  heroes = HEROES;
+  selectedHero: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: 'The Coon'
-  };
-}
-
-export class Hero {
-  id: number;
-  name: string;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
