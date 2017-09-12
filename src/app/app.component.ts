4b882e8a23a9a6d8c0e2948743e457d27dc5b585
José Aguilera <neojal@hotmail.com>
2017-09-12 12:27:21 -0500

    Parte 1 - [(ngModel)]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes - The SouthPark Series';
+
+  hero: Hero = {
+    id: 1,
+    name: 'The Coon'
+  };
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
