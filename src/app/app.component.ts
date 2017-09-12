76083ff4c78fc76723c7d33ee3400d860e3f1576
José Aguilera <neojal@hotmail.com>
2017-09-12 18:09:20 -0500

    Part4: Dependency Injection, Service, ngOnInit, Promises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,35 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  {id: 1, name: 'The Coon'},
-  {id: 2, name: 'Mysterion'},
-  {id: 3, name: 'Human Kite'},
-  {id: 4, name: 'Toolshed'},
-  {id: 5, name: 'Mosquito'},
-  {id: 6, name: 'Tupperware'},
-  {id: 7, name: 'Iron Maiden'},
-  {id: 8, name: 'Mint-Berry Crunch'}
-];
-
+import { HeroService} from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes - The SouthPark Series';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(
+      heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 }
+
+
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'The Coon'},
+  {id: 2, name: 'Mysterion'},
+  {id: 3, name: 'Human Kite'},
+  {id: 4, name: 'Toolshed'},
+  {id: 5, name: 'Mosquito'},
+  {id: 6, name: 'Tupperware'},
+  {id: 7, name: 'Iron Maiden'},
+  {id: 8, name: 'Mint-Berry Crunch'}
+];
