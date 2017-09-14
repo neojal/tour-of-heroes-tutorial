e3f9d3e05c6bd6132ce3f6a2454f87cbc28bf6e7
José Aguilera <neojal@hotmail.com>
2017-09-14 11:50:47 -0500

    Part5 - testing...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     RouterModule.forRoot([
       { path: 'heroes', component: HeroesComponent },
       { path: 'dashboard', component: DashboardComponent },
+      { path: 'detail/:id', component: HeroDetailComponent },
       { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
     ])
   ],
--- src/app/heroes/hero-detail.component.ts
+++ src/app/heroes/hero-detail.component.ts
@@ -1,11 +1,35 @@
-import { Component, Input } from '@angular/core';
-import { Hero} from './shared/hero';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap} from '@angular/router';
+import { Location } from '@angular/common';
+
+import 'rxjs/add/operator/switchMap';
+
+import { Hero } from './shared/hero';
+import { HeroService} from './shared/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html'
 })
 
-export class HeroDetailComponent {
-  @Input() hero: Hero;
+export class HeroDetailComponent implements OnInit{
+
+  hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.switchMap(
+      (params: ParamMap) =>
+      this.heroService.getHero(+params.get('id')))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
